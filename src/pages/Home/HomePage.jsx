3cd936569d4a58a0cd6b745dca28d5d3989351d5
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-05-06 16:43:07 +0800

    feat: create page route and styles file

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+import './HomePage.module.scss'
+
+function HomePage() {
+  return (
+    <>
+      <div>
+        <h1>Navigating the digital landscape for success</h1>
+        <p>Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
