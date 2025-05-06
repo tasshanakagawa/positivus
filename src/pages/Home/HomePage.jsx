8bab07efa800fdefeb3c6640a9b375418168b394
Tassha Nakagawa <nakagawatassha@gmail.com>
2025-05-06 20:06:56 +0800

    feat: add hero banner styles

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,13 +1,24 @@
-import './HomePage.module.scss'
+import './HomePage.scss'
+import Button from '../../components/Button/Button.jsx'
 
 function HomePage() {
   return (
-    <>
-      <div>
-        <h1>Navigating the digital landscape for success</h1>
-        <p>Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+    <div className="heroBanner">
+      <div className="container">
+        <div className="heroBanner__content">
+          <h1 className="content__title">Navigating the digital landscape for success</h1>
+          <p className="content__description">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+          <Button 
+            text="Book a consultation"
+            variant="primary"
+            to="http://google.com"
+          />
+        </div>
+        <div className="heroBanner__image">
+          <img src="../../../public/hero-banner-illustration.svg" alt="Hero Banner Image" />
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
